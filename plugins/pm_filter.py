8acf47e92fe9c97e1c81a07be5295486289dc971
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-30 06:37:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -419,15 +419,25 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url='http://t.me/Movie_Roster_bot?startgroup=true
-            ]'[
+ 
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url='http://t.me/Movie_Roster_Bot?startgroup=true
+
+            ],[
+
             InlineKeyboardButton('✨ ᴡᴏʀᴋɪɴɢ ɢʀᴏᴜᴘ ✨', url='https://t.me/MovieRosterGroup')
+
             ],[
-            InlineKeyboardButton('👮‍♂ ᴅᴇᴠᴇʟᴏᴘᴇʀ 👮‍♂', url='https://t.me/Rafeeq_Kunnimon'),
+
+            InlineKeyboardButton('🕵‍♂ ᴅᴇᴠᴇʟᴏᴘᴇʀ 🕵‍♂', url='https://t.me/Rafeeq_Kunnimon'),
+
             InlineKeyboardButton('❤️ sᴜᴘᴘᴏʀᴛ ❤️', url='https://t.me/MovieRosterOfficial')
+
             ],[
+
             InlineKeyboardButton('📚 ʜᴇʟᴘ 📚', callback_data='help'),
+
             InlineKeyboardButton('🔰 ᴀʙᴏᴜᴛ 🔰', callback_data='about')
+
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
