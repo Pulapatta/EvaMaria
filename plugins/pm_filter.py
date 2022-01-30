a1cc49f2bcb9305fdca07fb267c81dd6aec0fe1a
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-30 08:28:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -447,19 +447,19 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🧾 ᴍᴇᴍᴇs 🧾', callback_data='memes'),
             ],[
             InlineKeyboardButton('🔖 ᴘᴀsᴛᴇ 🔖', callback_data='paste'),
-            InlineKeyboardButton('📟 ᴘᴀssᴡᴏʀᴅ', callback_data='genpassword'),
+            InlineKeyboardButton('📟 ᴘᴀssᴡᴏʀᴅ ɢᴇɴ', callback_data='genpassword'),
             InlineKeyboardButton('📌 ᴘɪɴ 📌', callback_data='pin'),
             ],[
             InlineKeyboardButton('🎧 ᴘᴜʀɢᴇ 🎧', callback_data='purge'),
             InlineKeyboardButton('🎯 ʀᴇsᴛʀɪᴄ 🎯', callback_data='restric'),
             InlineKeyboardButton('🔎 sᴇᴀʀᴄʜ 🔍', callback_data='search'),
             ],[
-            InlineKeyboardButton('⭕ sʜᴀʀᴇ ᴛx⭕', callback_data='sharetext'),
+            InlineKeyboardButton('⭕ sʜᴀʀᴇ ᴛᴇxᴛ', callback_data='sharetext'),
             InlineKeyboardButton('🎶 ᴍᴜsɪᴄ 🎶', callback_data='music'),
             InlineKeyboardButton('🎵 ᴛᴛ-sᴘᴇᴇᴄʜ', callback_data='tts'),
             ],[
             InlineKeyboardButton('📋 ᴛɢʀᴀᴘʜ 📋', callback_data='tgraph'),
-            InlineKeyboardButton('🔸sʜᴏʀᴛɴᴇʀ🔹', callback_data='shortner'),
+            InlineKeyboardButton('🔸 ᴛᴇxᴛ sʜᴏʀᴛɴᴇʀ', callback_data='shortner'),
             InlineKeyboardButton('🧟 ᴢᴏᴍʙɪᴇs 🧟', callback_data='zombies'),
             ],[
             InlineKeyboardButton('✖️ ʙᴀᴄᴋ ✖️', callback_data='start')
